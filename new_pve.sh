3e4ba0a22ea9400993eebe64b7e8bef2ff46d910
panchuz <139817605+panchuz@users.noreply.github.com>
2024-05-02 13:22:23 -0300

    Create new_pve.sh
    “You do not have a valid subscription for this server….”

---
+++ new_pve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# creado por panchuz para evitar “You do not have a valid subscription for this server….”
+# https://dannyda.com/2020/05/17/how-to-remove-you-do-not-have-a-valid-subscription-for-this-server-from-proxmox-virtual-environment-6-1-2-proxmox-ve-6-1-2-pve-6-1-2/
+
+sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+systemctl restart pveproxy.service
+
+
+
+
+apt install btrfs-compsize
